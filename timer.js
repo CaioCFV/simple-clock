c84a11b0eb17bb516aced3dc287ca2092efca5cf
caiocfv <caio2011dhd@gmail.com>
2020-12-30 14:50:50 -0300

    Backup

---
+++ timer.js
@@ -0,0 +1,39 @@
+(function(){
+    const pointerHourElement   = document.querySelector('.pointer-hour');
+    const pointerMinuteElement = document.querySelector('.pointer-minute');
+    const pointerSecondElement = document.querySelector('.pointer-second');
+
+    const INITIAL_POINTER_ROTATE    = 45;
+    const SECOND_ROTATE_EQUIVALENT  = 6;
+    const MINUTE_ROTATE_EQUIVALENT  = 6; 
+    const HOUR_ROTATE_EQUIVALENT    = 60;
+
+    function calcSecondsToRotate(date){
+        return (date.getSeconds() * SECOND_ROTATE_EQUIVALENT) + INITIAL_POINTER_ROTATE;
+    };
+
+    function calcMinutesToRotate(date){
+        return (date.getMinutes() * MINUTE_ROTATE_EQUIVALENT) + INITIAL_POINTER_ROTATE;
+    };
+
+    function calcHoursToRotate(date){
+        if(date.getHours() > 12){
+            return (((HOUR_ROTATE_EQUIVALENT * (date.getHours() - 12)) - (HOUR_ROTATE_EQUIVALENT - date.getMinutes())));
+        }else{
+            return (((HOUR_ROTATE_EQUIVALENT * date.getHours()) - (HOUR_ROTATE_EQUIVALENT - date.getMinutes())) / 2) + INITIAL_POINTER_ROTATE;
+        }
+    };
+
+    setInterval(()=>{
+        const now = new Date();
+        pointerHourElement.style.transform   = `translate(-50%, -50%) rotate(${calcHoursToRotate(now)}deg)`;
+    //    pointerMinuteElement.style.transform = `translate(-50%, -50%) rotate(${calcMinutesToRotate(now)}deg)`;
+    //    pointerSecondElement.style.transform = `translate(-50%, -50%) rotate(${calcSecondsToRotate(now)}deg)`;
+    },1000);
+
+})();
+// 5 * 60 = 300
+// 285 / 2 = 190
+// 12 * 60 = 720
+// 720 - 20 = 700
+// 700 / 2 = 350
